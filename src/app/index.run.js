5ce4ed3e9aa629b784168c3d0a3bfec53597a4bb
Dirk <dirkjan@degroot.in>
2016-03-18 17:20:53 +0100

    new stuff

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -13,11 +13,18 @@
     });
 
 
-    if(!$rootScope.locale && $window.location.pathname === '/') {
+    if(!$rootScope.locale && $window.location.pathname === '/' || $window.location.pathname === '/en') {
       $rootScope.locale = 'english';
       $translate.use($rootScope.locale);
       $window.location = '/english';
     }
+
+    if(!$rootScope.locale && $window.location.pathname === '/cn') {
+      $rootScope.locale = 'chinese';
+      $translate.use($rootScope.locale);
+      $window.location = '/chinese';
+    }
+
 
     $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
       if($stateParams.locale && ['english','nederlands','chinese'].indexOf($stateParams.locale) !== -1){
