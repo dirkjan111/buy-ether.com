b66793ea475f2e57945792297907a8c78e4cbf09
Dirk <dirkjan@degroot.in>
2016-03-23 16:21:28 +0100

    chinese

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -33,14 +33,15 @@
           controller: 'MainController',
           controllerAs: 'vm',
           data: {
-            english : {
-              pageTitle : 'Ethereum wallet installation',
-              pageDescription : 'Manual Ethereum wallet installation and performing ether to bitcoin exchange'}
+            english: {
+              pageTitle: 'Ethereum wallet installation',
+              pageDescription: 'Manual Ethereum wallet installation and performing ether to bitcoin exchange'
             },
-            chinese : {
-              pageTitle : '购买 Ether (以太坊 Ethereum)',
-              pageDescription : '以太坊(ETH)是一种可交易的资产, 很快你将可以从这里购买到以太坊.'
+            chinese: {
+              pageTitle: '指南 - 以太坊(Ethereum)钱包安装教程以及使用以太交易小部件购买以太坊',
+              pageDescription: '这个教程展示以太坊(Ethereum)官方钱包Mist的安装步骤以及如何使用比特币购买以太坊。'
             }
+          }
         });
 
 
