6f7445e0eda6a2bdfbd4f21ca7ff355cce7d0e9d
Dirk <dirkjan@degroot.in>
2016-05-25 23:24:47 +0200

    add max

--- src/app/content/manual.controller.js
+++ src/app/content/manual.controller.js
@@ -10,6 +10,8 @@
 
       $scope.baseUrl = 'https://swap.innocoin.com/#/';
 
+      $scope.max = 2.5;
+
       $scope.iframe1 =  $sce.trustAsResourceUrl($scope.baseUrl + '?language=english&partner_email=partner@innocoin.com&partner_address=3BfKMdyXzG8e8oHCb6YSGVRdASZ3XW6xni&partner_margin=1.25&source=BTC&target=ETH&target_amount=1&show_info=true&theme=concrete_wall&rounded_corners=true&border=false&iframe=true');
       $scope.iframe2 =  $sce.trustAsResourceUrl($scope.baseUrl + '?language=chinese&partner_email=partner@innocoin.com&partner_address=3BfKMdyXzG8e8oHCb6YSGVRdASZ3XW6xni&partner_margin=1.25&source=BTC&target=ETH&target_amount=1&show_info=true&theme=random_grey_variations&rounded_corners=true&border=false&iframe=true');
 
